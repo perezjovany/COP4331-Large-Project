c3b7c7c8e924460a313a172131b7799d803ae4f6
shamada05 <137637055+shamada05@users.noreply.github.com>
2023-07-12 14:57:48 -0400

    Update api.js

--- api.js
+++ api.js
@@ -3,28 +3,25 @@ require('mongodb');
 
 //load user model
 const User = require("./models/user.js");
-//load card model
-const Card = require("./models/card.js");
 
 exports.setApp = function ( app, client )
 {
 
-  app.post('/api/addcard', async (req, res, next) =>
+  app.post('/api/signup', async (req, res, next) =>
   {
-    // incoming: userId, card
+    // incoming: firstName, lastName, login, password, email, phone
     // outgoing: error
     
-    const { userId, card } = req.body;
+    const { FirstName, LastName, Login, Password, Email, Phone } = req.body;
 
-    //const newCard = { Card: card, UserId: userId };
-    const newCard = new Card({ Card: card, UserId: userId });
+    const newUser = new User({ firstName: FirstName, lastName: LastName, login: Login, password: Password, email: Email, phone: Phone });
     var error = '';
     
     try 
     {
-      // const db = client.db();
-      // const result = db.collection('Cards').insertOne(newCard);
-      newCard.save();
+      const db = client.db();
+      const result = db.collection('Users').insertOne(newUser);
+      newUser.save();
     }
     catch(e)
     {
@@ -39,131 +36,33 @@ exports.setApp = function ( app, client )
   app.post('/api/login', async (req, res, next) => 
   {
     // incoming: login, password
-    // outgoing: id, firstName, lastName, error
+    // outgoing: id, firstName, lastName, email, phone, error
     
     var error = '';
 
     const { login, password } = req.body;
 
-    // const db = client.db("COP4331Cards");
-    // const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-    const results = await User.find({ Login: login, Password: password });
+    //const db = client.db("COP4331Food");
+    //const results = await db.collection('Users').find({login: login, password: password}).toArray();
+    const results = await User.find({ login: login, password: password });
 
     var id = -1;
     var fn = '';
     var ln = '';
+    var em = '';
+    var ph = '';
 
     if( results.length > 0 )
     {
-      id = results[0].UserID;
-      fn = results[0].FirstName;
-      ln = results[0].LastName;
+      id = results[0].userID;
+      fn = results[0].firstName;
+      ln = results[0].lastName;
+      em = results[0].email;
+      ph = results[0].phone;
     }
 
-    var ret = { id:id, firstName:fn, lastName:ln, error:''};
+    var ret = { id:id, firstName:fn, lastName:ln, email:em, phone:ph, error: error};
     res.status(200).json(ret);
   });
 
-  app.post('/api/searchcards', async (req, res, next) => 
-  {
-    // incoming: userId, search
-    // outgoing: results[], error
-
-    var error = '';
-
-    const { userId, search } = req.body;
-
-    var _search = search.trim();
-    
-    // const db = client.db("COP4331Cards");
-    // const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'i'}}).toArray();
-    const results = await Card.find({ "Card": { $regex: _search + '.*', $options: 'i' } });
-    
-    var _ret = [];
-    for( var i=0; i<results.length; i++ )
-    {
-      _ret.push( results[i].Card );
-    }
-    
-    var ret = {results:_ret, error:error};
-    res.status(200).json(ret);
-  });
-    
 }
-
-
-// app.post('/api/addcard', async (req, res, next) =>
-// {
-//   // incoming: userId, card
-//   // outgoing: error
-	
-//   const { userId, card } = req.body;
-
-//   const newCard = {Card:card,UserId:userId};
-//   var error = '';
-
-//   try
-//   {
-//     const db = client.db("COP4331Cards");
-//     const result = db.collection('Cards').insertOne(newCard);
-//   }
-//   catch(e)
-//   {
-//     error = e.toString();
-//   }
-
-//   var ret = { error: error };
-//   res.status(200).json(ret);
-// });
-
-
-// app.post('/api/login', async (req, res, next) => 
-// {
-//   // incoming: login, password
-//   // outgoing: id, firstName, lastName, error
-	
-//  var error = '';
-
-//   const { login, password } = req.body;
-
-//   const db = client.db("COP4331Cards");
-//   const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-
-//   var id = -1;
-//   var fn = '';
-//   var ln = '';
-
-//   if( results.length > 0 )
-//   {
-//     id = results[0].UserID;
-//     fn = results[0].FirstName;
-//     ln = results[0].LastName;
-//   }
-
-//   var ret = { id:id, firstName:fn, lastName:ln, error:''};
-//   res.status(200).json(ret);
-// });
-
-// app.post('/api/searchcards', async (req, res, next) => 
-// {
-//   // incoming: userId, search
-//   // outgoing: results[], error
-
-//   var error = '';
-
-//   const { userId, search } = req.body;
-
-//   var _search = search.trim();
-  
-//   const db = client.db("COP4331Cards");
-//   const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'i'}}).toArray();
-  
-//   var _ret = [];
-//   for( var i=0; i<results.length; i++ )
-//   {
-//     _ret.push( results[i].Card );
-//   }
-  
-//   var ret = {results:_ret, error:error};
-//   res.status(200).json(ret);
-// });
