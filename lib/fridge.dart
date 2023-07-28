eb7d095d8afd6ca40459209ae18003943d744a53
lencasser <134425118+lencasser@users.noreply.github.com>
2023-07-28 16:28:51 -0400

    added fridge and search_food dart files

---
+++ lib/fridge.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/search_food.dart';
+import 'package:flutter_app/top_bar.dart';
+
+// TODO
+// - connect to DB
+// - allow to add food to list
+// - allow to show nutrition facts
+// - connect to main
+// - make food result page
+// - test with emulator (couldnt get emulator to work, was stuck on login page) 
+
+class FridgePage extends StatefulWidget {
+  const FridgePage({super.key});
+
+  @override
+  State<FridgePage> createState() => _FridgePageState();
+}
+
+class _FridgePageState extends State<FridgePage> {
+  String selectedFood = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const topBar(title: 'Fridge'),
+      body: Column(children: [
+        ElevatedButton(
+          onPressed: () async {
+            final finalResult = await showSearch(
+              context: context,
+              delegate: SearchFood(
+                foodItems: allfood,
+              ),
+            );
+            setState(() {
+              selectedFood = finalResult!;
+            });
+          },
+          child: const Text('Search'),
+        ),
+      ]),
+    );
+  }
+}
+
+//example list, need to connect to db
+List<String> allfood = [
+  'chicken',
+  'yams',
+  'mango',
+  'cake',
+  'pineapple',
+  'apple',
+  'apple sauce',
+  'grapes',
+  'apple juice'
+];
