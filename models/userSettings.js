cff92505e40f336e11cd4d2a0a2678ce41e5ab8b
Jovany Perez <perezjovany@gmail.com>
2023-07-28 20:50:45 -0400

    fixed userSettings model

--- models/userSettings.js
+++ models/userSettings.js
@@ -26,4 +26,4 @@ const UserSchema = new Schema({
   }
 });
 
-module.exports = list = mongoose.model("list_items", UserSchema, "list_items");
+module.exports = list = mongoose.model("user_settings", UserSchema, "user_settings");
